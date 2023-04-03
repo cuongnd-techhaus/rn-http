77b2521819ef5461652ed8d09d44fd7d3aa75406
duycuongit <118497439+duycuongit@users.noreply.github.com>
2023-04-03 10:32:27 +0700

    learn http

---
+++ rn_http/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import HttpExample from './http_example.js'
+
+const App = () => {
+   return (
+      <HttpExample />
+   )
+}
+export default App
